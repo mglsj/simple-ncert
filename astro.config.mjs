ec825893083cb4366dcf07dd8a55011c9303e37b
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-12-16 20:13:42 +0530

    class 11 cs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import rehypeMermaid from "rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
 	markdown: {
 		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
+		rehypePlugins: [
+			rehypeKatex,
+			[rehypeMermaid, { strategy: "img-svg", dark: true }],
+		],
 	},
 	site: "https://mglsj.github.io/",
 	base: "/simple-ncert/",
